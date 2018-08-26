e234b70ed88ca1227886e02593f1d2c687c33b52
Alvin Jay C. Cosare <alvinjaycosare@gmail.com>
2018-08-26 17:50:42 +0800

    [✨] Implement QuoteEmail model

---
+++ Eagle Quote Alvin/Models/QuoteEmail.swift
@@ -0,0 +1,44 @@
+//
+//  QuoteEmail.swift
+//  Eagle Quote Alvin
+//
+//  Created by Alvin Jay C. Cosare on 26/08/2018.
+//  Copyright © 2018 BlackFin. All rights reserved.
+//
+
+import Foundation
+
+struct QuoteEmail: Codable {
+  
+  let recipients: [QuoteEmail.Recipient]
+  let replyTo: String
+  let subject: String
+  let body: String
+  let quoteId: Int
+  
+  enum CodingKeys: String, CodingKey {
+    case recipients
+    case replyTo = "replyto"
+    case subject
+    case body
+    case quoteId
+  }
+  
+}
+
+extension QuoteEmail {
+  
+  struct Recipient: Codable {
+    
+    enum InputType: String, Codable {
+      case to
+      case cc
+    }
+    
+    let email: String
+    let name: String
+    let type: InputType
+    
+  }
+  
+}
