2c96e1592dd9ff6180bdce338a4c1e79350768d0
Alvin Jay C. Cosare <alvinjaycosare@gmail.com>
2018-08-26 14:58:57 +0800

    [✨] Implement Client model

---
+++ Eagle Quote Alvin/Models/Client.swift
@@ -0,0 +1,25 @@
+//
+//  Client.swift
+//  Eagle Quote Alvin
+//
+//  Created by Alvin Jay C. Cosare on 26/08/2018.
+//  Copyright © 2018 BlackFin. All rights reserved.
+//
+
+import Foundation
+
+struct Client: Codable {
+  
+  let id: Int
+  let name: String
+  let gender: String
+  let age: Int
+  let occupationId: Int
+  let employedStatus: String
+  let isSmoker: Bool
+  let isChild: Bool
+  let isPrimary: Bool
+  let premiums: Premiums
+  
+  
+}
