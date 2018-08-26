29b4b35ddbf74b3c613db8d4bbb2d49d3afaf323
Alvin Jay C. Cosare <alvinjaycosare@gmail.com>
2018-08-26 17:08:57 +0800

    [✨] Implement User model

---
+++ Eagle Quote Alvin/Models/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  Eagle Quote Alvin
+//
+//  Created by Alvin Jay C. Cosare on 26/08/2018.
+//  Copyright © 2018 BlackFin. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+  
+  let id: String
+  let email: String
+  let fspr: String
+  let fullName: String
+  let createdAt: String
+  let updatedAt: String
+  
+}
