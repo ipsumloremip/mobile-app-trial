3a6ea825e7d548f32e5a2cb21f65872b71a3e00a
Alvin Jay C. Cosare <alvinjaycosare@gmail.com>
2018-08-26 19:50:47 +0800

    [✨] Implement SessionService

---
+++ Eagle Quote Alvin/Services/SessionService.swift
@@ -0,0 +1,59 @@
+//
+//  SessionService.swift
+//  Eagle Quote Alvin
+//
+//  Created by Alvin Jay C. Cosare on 26/08/2018.
+//  Copyright © 2018 BlackFin. All rights reserved.
+//
+
+import Foundation
+
+import PluggableApplicationDelegate
+
+class SessionService: NSObject, ApplicationService {
+  
+  private struct Keys {
+    static let LoggedInUser = "SessionService:Keys:LoggedInUser"
+    static let AccessToken = "SessionService:Keys:AccessToken"
+  }
+  
+  
+  var hasSession: Bool {
+    guard let _ = user else {
+      return false
+    }
+    return true
+  }
+  
+  var token: String? {
+    get {
+      return UIApplication.shared.appDelegate.keychainService.get(Keys.AccessToken)
+    }
+    set(newAccessToken) {
+      UIApplication.shared.appDelegate.keychainService.set(securedValue: newAccessToken, key: Keys.AccessToken)
+    }
+  }
+  
+  private(set) var user: User? {
+    didSet {
+      guard let user = user else {
+        return UserDefaults.standard.set(nil, forKey: Keys.LoggedInUser)
+      }
+      
+      let wrappedUser = try! JSONEncoder().encode(user)
+      UserDefaults.standard.set(wrappedUser, forKey: Keys.LoggedInUser)
+      
+    }
+  }
+  
+  override init() {
+    if let userData = UserDefaults.standard.object(forKey: Keys.LoggedInUser) as? Data {
+      do {
+        self.user = try JSONDecoder().decode(User.self, from: userData)
+      } catch {
+        self.user = nil
+      }
+    }
+  }
+  
+}
