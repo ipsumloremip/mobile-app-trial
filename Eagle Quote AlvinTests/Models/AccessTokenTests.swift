e35fd9203826cdc450ad56ad1260ee0f68262b42
Alvin Jay C. Cosare <alvinjaycosare@gmail.com>
2018-08-26 13:53:18 +0800

    [🚨] Add unit test file AccessTokenTests

---
+++ Eagle Quote AlvinTests/Models/AccessTokenTests.swift
@@ -0,0 +1,16 @@
+//
+//  AccessTokenTests.swift
+//  Eagle Quote AlvinTests
+//
+//  Created by Alvin Jay C. Cosare on 26/08/2018.
+//  Copyright © 2018 BlackFin. All rights reserved.
+//
+
+import Foundation
+
+import Quick
+import Nimble
+
+class AccessTokenTests: QuickSpec {
+  
+}
