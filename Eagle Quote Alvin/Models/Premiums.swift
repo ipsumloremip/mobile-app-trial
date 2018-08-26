df8023bb379bd24e495481cf5e2ca83c728038de
Alvin Jay C. Cosare <alvinjaycosare@gmail.com>
2018-08-26 14:38:04 +0800

    [✨] Implement Premiums model

---
+++ Eagle Quote Alvin/Models/Premiums.swift
@@ -0,0 +1,16 @@
+//
+//  Premiums.swift
+//  Eagle Quote Alvin
+//
+//  Created by Alvin Jay C. Cosare on 26/08/2018.
+//  Copyright © 2018 BlackFin. All rights reserved.
+//
+
+import Foundation
+
+struct Premiums: Codable {
+  
+  let minTotalPremium: Double
+  let maxTotalPremium: Double
+  
+}
