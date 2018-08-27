dbe5c0f973503d8b9f6c6d8a0bdc4e2725062687
Alvin Jay C. Cosare <alvinjaycosare@gmail.com>
2018-08-27 21:13:26 +0800

    [🛠] Hook login vm to vc

--- Eagle Quote Alvin/Scenes/Login/LoginController.swift
+++ Eagle Quote Alvin/Scenes/Login/LoginController.swift
@@ -9,27 +9,129 @@
 import Foundation
 import UIKit
 
+import RxSwift
+import RxCocoa
+
 class LoginController: UIViewController {
 
   @IBOutlet weak var iconImageView: UIImageView!
   @IBOutlet weak var iconImageViewFullWidthALC: NSLayoutConstraint!
   @IBOutlet weak var iconImageViewSupressedWidthALC: NSLayoutConstraint!
-  
+
+  @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+
   @IBOutlet weak var signupStackViewZeroHeightALC: NSLayoutConstraint!
   @IBOutlet weak var signupButtonsStackView: UIStackView!
   @IBOutlet weak var signupButtonHeightALC: NSLayoutConstraint!
   @IBOutlet weak var signupStackViewBottomSpaceViewHeightALC: NSLayoutConstraint!
-  
-  @IBOutlet weak var loginStackViewZeroHeightALC: NSLayoutConstraint!
+
+  @IBOutlet var loginStackViewZeroHeightALC: NSLayoutConstraint!
   @IBOutlet weak var loginStackView: UIStackView!
 
+  @IBOutlet weak var signInButton: RoundedCornersButton!
   
-  @IBAction func signInButtonTapped(_ sender: Any) {
+  @IBOutlet weak var emailTextField: UITextField!
+  @IBOutlet weak var passwordTextField: UITextField!
 
+  var viewModel: LoginViewModelType!
+
+  private let animationDuration = 0.25
+  private let disposeBag = DisposeBag()
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    setup()
+    bind()
+  }
+  
+  private func setup() {
+    emailTextField.delegate = self
+    passwordTextField.delegate = self
+  }
+  
+  private func bind() {
+
+    emailTextField.rx.text
+      .bind(to: viewModel.input.email)
+      .disposed(by: disposeBag)
+    
+    passwordTextField.rx.text
+      .bind(to: viewModel.input.password)
+      .disposed(by: disposeBag)
+
+    signInButton.rx.tap
+      .bind(to: viewModel.input.login)
+      .disposed(by: disposeBag)
+
+    viewModel.output.loggingIn
+      .subscribe(onNext: { [weak self] _ in
+        self?.showLoadingUI()
+      })
+      .disposed(by: disposeBag)
+    
+    viewModel.output.didReceiveLoginError
+      .subscribe(onNext: { [weak self] _ in
+        self?.revertLoadingUI()
+      })
+      .disposed(by: disposeBag)
+
+  }
+  
+  @IBAction func showSignInButtonTapped(_ sender: Any) {
+    showSignInView()
+  }
+
+}
+
+extension LoginController {
+
+  fileprivate func showLoadingUI() {
+    
+    self.loginStackView.isHidden = true
+    NSLayoutConstraint.activate([loginStackViewZeroHeightALC])
+    
+    UIView.animate(withDuration: self.animationDuration, animations: {
+      self.view.layoutIfNeeded()
+    }) { _ in
+      
+      NSLayoutConstraint.deactivate([self.iconImageViewSupressedWidthALC])
+      NSLayoutConstraint.activate([self.iconImageViewFullWidthALC])
+      
+      UIView.animate(withDuration: self.animationDuration, animations: {
+        self.view.layoutIfNeeded()
+      }) { _ in
+        self.activityIndicator.startAnimating()
+      }
+    }
+    
+  }
+  
+  fileprivate func revertLoadingUI() {
+    
+    NSLayoutConstraint.deactivate([self.iconImageViewFullWidthALC])
+    NSLayoutConstraint.activate([self.iconImageViewSupressedWidthALC])
+    
+    self.activityIndicator.stopAnimating()
+    UIView.animate(withDuration: self.animationDuration, animations: {
+      self.view.layoutIfNeeded()
+    }) { _ in
+      
+      self.loginStackView.isHidden = false
+      NSLayoutConstraint.deactivate([self.loginStackViewZeroHeightALC])
+      
+      UIView.animate(withDuration: self.animationDuration, animations: {
+        self.view.layoutIfNeeded()
+      })
+    }
+    
+  }
+  
+  fileprivate func showSignInView() {
     self.iconImageViewFullWidthALC.isActive = false
     self.iconImageViewSupressedWidthALC.isActive = true
-
-    UIView.animate(withDuration: 0.35, animations: {
+    
+    UIView.animate(withDuration: self.animationDuration, animations: {
       self.signupButtonsStackView.alpha = 0
       self.view.layoutIfNeeded()
     }) { _ in
@@ -41,10 +143,19 @@ class LoginController: UIViewController {
       self.loginStackView.isHidden = false
       self.loginStackViewZeroHeightALC.isActive = false
       
-      UIView.animate(withDuration: 0.35, animations: {
+      UIView.animate(withDuration: self.animationDuration, animations: {
         self.view.layoutIfNeeded()
       })
     }
-    
   }
+
+}
+
+extension LoginController: UITextFieldDelegate {
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    textField.resignFirstResponder()
+    return false
+  }
+  
 }
